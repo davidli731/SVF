b974752e7ccbe43c3397c88b7f59a85a74bba729
David Li <davidli731@gmail.com>
2021-10-31 22:54:47 +1100

    Update ExtAPI.cpp

--- lib/Util/ExtAPI.cpp
+++ lib/Util/ExtAPI.cpp
@@ -196,7 +196,7 @@ void ExtAPI::init()
     {
       fputs(p->n, stderr);
       putc('\n', stderr);
-      assert(!"duplicate name in ei_pairs"+p->n);
+      assert(!"duplicate name in ei_pairss");
     }
     info[p->n] = p->t;
   }
